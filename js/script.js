f8b16b1e170f73f8eb3c6b73f0fe1103a4259ce1
DewanSingh <dewansingh805@gmail.com>
2024-03-04 23:37:09 -0800

    Update script.js
    correctly referenced function variable

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ function init(){
   //add your javascript between these two lines of code
   function testText(){
     var alertbox = document.getElementById('entryinput');
-    document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Dewan Singh: ", textbox.value);
+    document.getElementById('textoutput').innerHTML = alertbox.value;
+    alert("Dewan Singh: ", alertbox.value);
   }
 
 
