41c419ae3c5ef70e355bea73b053528f71cf759b
DewanSingh <dewansingh805@gmail.com>
2024-03-04 23:41:21 -0800

    Update script.js
    added variable for alert button and assigned it to a click event with the test function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 function init(){
   //add your javascript between these two lines of code
+  var alertbutton = document.getElementById('entrybutton');
+  
   function testText(){
     var alertbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = alertbox.value;
     alert("Dewan Singh: ", alertbox.value);
   }
-
-
-
-
-
+  
+  alertbutton.addEventListener('click', testText);
 }
 
 window.addEventListener('load', init);
